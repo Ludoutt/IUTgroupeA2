55754a406006bd9ddd261f566a053e504863cbcc
Nicolas BERTHOU <berthou.nicolas@laposte.net>
2019-03-12 16:52:57 +0100

    ADD entity for user and element

---
+++ src/Entity/Element.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ElementRepository")
+ */
+class Element
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $position;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $complexity;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbHours;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getPosition(): ?int
+    {
+        return $this->position;
+    }
+
+    public function setPosition(int $position): self
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+
+    public function getComplexity(): ?int
+    {
+        return $this->complexity;
+    }
+
+    public function setComplexity(int $complexity): self
+    {
+        $this->complexity = $complexity;
+
+        return $this;
+    }
+
+    public function getNbHours(): ?int
+    {
+        return $this->nbHours;
+    }
+
+    public function setNbHours(int $nbHours): self
+    {
+        $this->nbHours = $nbHours;
+
+        return $this;
+    }
+}
