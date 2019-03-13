ffaf4522cb2de02605c76da2490da41923a49a9d
Nicolas BERTHOU <berthou.nicolas@laposte.net>
2019-03-13 11:47:58 +0100

    ADD crud for User entity

---
+++ src/Form/UserType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType implements FormTypeInterface
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Product Owner' => 'ROLE_PRODUCT_OWNER',
+                    'Developer' => 'ROLE_DEV'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
